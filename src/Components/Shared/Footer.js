1d3ae732a79c48763abd353963a626f3950bfe01
Jorge Mendez <mendezjorge98@gmail.com>
2022-06-14 01:50:19 -0300

    added footer styles

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,13 +1,21 @@
 import styles from './Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <>
-      <footer className={styles.Footer}>
-        <div className={styles.Footer__container}>
-          <h3 className={styles.Footer__container__title}></h3>
-          <div className={styles.Footer__container__icons}>
-              
+      <footer className={styles.footer}>
+        <div className={styles.footer__container}>
+          <h3 className={styles.footer__container__title}>Puede seguirme en </h3>
+          <div className={styles.footer__container__icons}>
+            <a className={styles.footer__container__icons__a} href='https://www.instagram.com/jorgem_088/?hl=es-la' target={'_blank'} rel="noreferrer"  >
+              <FontAwesomeIcon className={styles.icon} icon={faInstagram} ></FontAwesomeIcon>
+            </a>
+            <a className={styles.footer__container__icons__a} href='https://github.com/jorge088' target={'_blank'} rel="noreferrer" >
+              <FontAwesomeIcon className={styles.icon} icon={faGithub}></FontAwesomeIcon>
+            </a>
           </div>
         </div>
       </footer>
