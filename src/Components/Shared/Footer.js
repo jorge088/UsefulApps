9ab4d88526307605517ed3702884a88004e3a7fa
Jorge Mendez <mendezjorge98@gmail.com>
2022-08-10 19:11:33 -0300

    change styles for footer component

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <>
       <footer className={styles.footer}>
         <div className={styles.footer__container}>
-          <h3 className={styles.footer__container__title}>Puede seguirme en </h3>
+          <h3 className={styles.footer__container__title}>Seguime en </h3>
           <div className={styles.footer__container__icons}>
             <a className={styles.footer__container__icons__a} href='https://www.instagram.com/jorgem_088/?hl=es-la' target={'_blank'} rel="noreferrer"  >
               <FontAwesomeIcon className={styles.icon} icon={faInstagram} ></FontAwesomeIcon>
