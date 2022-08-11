b49fd8fec0f2fe8f9772abe126f9fce6daf241cc
Jorge Mendez <mendezjorge98@gmail.com>
2022-08-11 18:19:54 -0300

    added validations for contactForm

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,38 +6,59 @@ const Contact = () => {
 
     const form = useRef();
 
+    const validateEmail = (email) => {
+        return email.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    };
+
     const sendEmail = (e) => {
         e.preventDefault();
+        let target = e.target;
+        let name = target.name.value;
+        let email = target.email.value;
+        let message = target.email.value;
 
-        emailjs.sendForm('service_5wdohco', 'template_79kck89', form.current, 'jbaqcmYR0pfEweOpg')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        if (name == '' || email == '' || message == '') {
+            alert('Hay campos incompletos');
+            return;
+        }
+        
+        if (validateEmail(email)) {
+            emailjs.sendForm('service_5wdohco', 'template_79kck89', form.current, 'jbaqcmYR0pfEweOpg')
+                .then((result) => {
+                    alert('Enviado correctamente');
+                }, (error) => {
+                    alert('Hubo un error al enviar');
+                });
+        }else{
+            alert('Invalid Email');
+            return;
+        }
+
     };
 
     return (
         <>
             <div className={styles.container}>
                 <form ref={form} onSubmit={sendEmail} className={styles.formContact} >
-                    <input 
-                        type="text" 
-                        name="name" 
-                        placeholder='Name' 
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder='Name'
                         className={styles.formContact__input} />
-                    <input 
-                        type="email" 
-                        name="email" 
-                        placeholder='Email' 
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder='Email'
                         className={styles.formContact__input} />
-                    <textarea 
-                        name="message" 
-                        placeholder='Message' 
-                        className={styles.formContact__textarea}/>
-                    <input 
-                        type="submit" 
-                        value="Send" 
+                    <textarea
+                        name="message"
+                        placeholder='Message'
+                        className={styles.formContact__textarea} />
+                    <input
+                        type="submit"
+                        value="Send"
                         className={styles.formContact__submit} />
                 </form>
             </div>
