62f7ad4a1a63e46ca23f34fd36cd5a708999e89b
Jorge Mendez <mendezjorge98@gmail.com>
2022-12-07 13:20:29 -0300

    Created settingsForm and added styles

--- src/features/pomodoroApp/PomodoroSettings.js
+++ src/features/pomodoroApp/PomodoroSettings.js
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux';
+import { getAllSettings } from './pomodoroSlice';
 import { useState } from 'react';
+
+
 import styles from './PomodoroSettings.module.css';
 
 
 const PomodoroSettings = ({ _callbackCloseSettings }) => {
     const [showDisappearAnimation, setShowDisappearAnimation] = useState(false);
+    const settings = useSelector(getAllSettings);
 
     const handleCloseSettings = (e) => {
         if (e.target.id === 'container') {
@@ -21,9 +26,33 @@ const PomodoroSettings = ({ _callbackCloseSettings }) => {
             className={styles.container}
             onClick={handleCloseSettings}>
 
-            <div 
+            <div
                 className={`${styles.settingsContainer} ${showDisappearAnimation ? styles.disappear : ''}`}>
-                <p>settings</p>
+                <h2>Preferencias</h2>
+                <h3>Cronómetro</h3>
+                <form className={styles.formSettings}>
+                    <div className={styles.formSettings__section}>
+                        <label>Duración del Pomodoro:</label>
+                        <input
+                            type='number'
+                            value={settings.work}
+                        />
+                    </div>
+                    <div className={styles.formSettings__section}>
+                        <label>Duración del descanso:</label>
+                        <input
+                            type='number'
+                            value={settings.break}
+                        />
+                    </div>
+                    <div className={styles.formSettings__section}>
+                        <label>Duración del descanso largo:</label>
+                        <input
+                            type='number'
+                            value={settings.long}
+                        />
+                    </div>
+                </form>
             </div>
 
         </div>
